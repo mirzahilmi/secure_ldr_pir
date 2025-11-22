515270e6ba4ce0438715ad48b27367d166cff7f0
Syahreza-Ferdian <syahrezafistiferdian32@gmail.com>
2025-11-23 01:23:27 +0700

    `feat`: add compile-time env parameters

--- embedded/build.rs
+++ embedded/build.rs
@@ -1,3 +1,18 @@
+use std::env;
+use dotenvy::from_path;
+
 fn main() {
+    let _ = from_path(".env");
+
+    println!("cargo:rerun-if-changed=.env");
+    
+    if let Ok(ssid) = env::var("WIFI_SSID") {
+        println!("cargo:rustc-env=WIFI_SSID={}", ssid);
+    }
+
+    if let Ok(password) = env::var("WIFI_PASSWORD") {
+        println!("cargo:rustc-env=WIFI_PASSWORD={}", password);
+    }
+
     embuild::espidf::sysenv::output();
 }

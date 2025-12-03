93da1e8f613f03ae2400b18c7a82e2a91872691a
mirza@t4nix <dev@mrz.my.id>
2025-12-03 19:48:54 +0700

    build: embed rust crypto lib under golang src

--- broker/go.mod
+++ broker/go.mod
@@ -3,22 +3,42 @@ module github.com/mirzahilmi/secure_ldr_pir/broker
 go 1.25.2
 
 require (
-	github.com/cloudflare/circl v1.6.1
 	github.com/coreos/go-oidc/v3 v3.16.0
 	github.com/danielgtaylor/huma/v2 v2.34.1
+	github.com/eclipse/paho.mqtt.golang v1.5.1
 	github.com/go-chi/chi/v5 v5.2.3
-	github.com/gorilla/websocket v1.5.3
+	github.com/goccy/go-json v0.10.5
 	github.com/rs/zerolog v1.34.0
 	github.com/spf13/cobra v1.9.1
-	golang.org/x/crypto v0.38.0
+	go.opentelemetry.io/otel v1.38.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.38.0
+	go.opentelemetry.io/otel/metric v1.38.0
+	go.opentelemetry.io/otel/sdk v1.38.0
+	go.opentelemetry.io/otel/sdk/metric v1.38.0
 )
 
 require (
+	github.com/cenkalti/backoff/v5 v5.0.3 // indirect
 	github.com/go-jose/go-jose/v4 v4.1.3 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	golang.org/x/oauth2 v0.28.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.7.1 // indirect
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250825161204-c5933d9347a5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250825161204-c5933d9347a5 // indirect
+	google.golang.org/grpc v1.75.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
 )
---
+++ broker/internal/iot/port/mqtt.go
@@ -0,0 +1,98 @@
+package port
+
+import (
+	"bytes"
+	"context"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/goccy/go-json"
+	"github.com/mirzahilmi/secure_ldr_pir/broker/internal/common/crypto"
+	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
+)
+
+type mqttHandler struct {
+	// im not sure we supposed to put context here or if there's any better solution
+	ctx      context.Context
+	ldrGauge metric.Int64Gauge
+	pirGauge metric.Int64Gauge
+}
+
+func NewMqttHandlers(ctx context.Context) ([]func() (string, byte, func(mqtt.Client, mqtt.Message)), error) {
+	meter := otel.Meter("iot")
+
+	ldrGauge, err := meter.Int64Gauge(
+		"sensors.ldr",
+		metric.WithDescription("LDR Sensor Reading"),
+		metric.WithUnit("Ohms"),
+	)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("iot: cannot create meter gauge instance")
+		return nil, err
+	}
+	pirGauge, err := meter.Int64Gauge(
+		"sensors.ldr",
+		metric.WithDescription("PIR Sensor Reading"),
+		metric.WithUnit("Boolean"),
+	)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Msg("iot: cannot create meter gauge instance")
+		return nil, err
+	}
+
+	h := mqttHandler{ctx, ldrGauge, pirGauge}
+
+	handlers := []func() (string, byte, func(mqtt.Client, mqtt.Message)){
+		func() (string, byte, func(mqtt.Client, mqtt.Message)) {
+			return "esp32/kriptografi/encrypted/ldr-pir", 0, h.OnReading
+		},
+	}
+	return handlers, nil
+}
+
+func (h *mqttHandler) OnReading(_ mqtt.Client, message mqtt.Message) {
+	encryptedReading := new(EncryptedReading)
+	if err := json.NewDecoder(bytes.NewBuffer(message.Payload())).Decode(encryptedReading); err != nil {
+		log.Error().
+			Err(err).
+			Bytes("message", message.Payload()).
+			Msg("iot: cannot decode mqtt message to reading struct")
+		return
+	}
+
+	plaintext, err := crypto.EnvelopeUnseal(
+		encryptedReading.Ciphertext,
+		encryptedReading.Nonce,
+		encryptedReading.Header.EphemeralPublicKey,
+	)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Bytes("message", message.Payload()).
+			Msg("iot: cannot decode mqtt message to reading struct")
+		return
+	}
+
+	var reading Reading
+	if err := json.NewDecoder(bytes.NewBufferString(plaintext)).Decode(&reading); err != nil {
+		log.Error().
+			Err(err).
+			Bytes("message", message.Payload()).
+			Msg("iot: cannot decode mqtt message to reading struct")
+		return
+	}
+	h.ldrGauge.Record(h.ctx, int64(reading.Ldr))
+	h.pirGauge.Record(h.ctx, int64(func() int64 {
+		if reading.Pir {
+			return 1
+		}
+		return 0
+	}()))
+
+	log.Info().Str("data", plaintext).Msg("iot: unencrypted received message")
+}
